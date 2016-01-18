5051b6243439b3731424967e6e2b5ab1388d2cde
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2016-01-18 13:33:01 +0100

    Remove the StatController and use the EventController

---
+++ app/controllers/event.scala
@@ -0,0 +1,36 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import java.time.ZonedDateTime
+import java.util.UUID
+
+import models._
+
+class EventController extends Controller {
+
+  def addEvent = Action.async(parse.json) { req =>
+    val ip = Ip.removeLastBytes(req.remoteAddress).getOrElse("127.0.0.1")
+    val userAgent = req.headers.get("User-Agent")
+    val datetime = ZonedDateTime.now
+
+    val o_input = EventInput(req.body)
+
+    val o_event = o_input.map {
+      input => Event.fromEventInput(input, ip, userAgent, datetime)
+    }
+
+    val o_document = o_event.map {
+      event => MongoConversions.fromEvent(event, UUID.randomUUID)
+    }
+
+    o_document.fold[Future[Result]](Future(BadRequest)) {
+      doc => Mongo.insertOne(doc).map(_ => NoContent)
+    }
+  }
+
+}
